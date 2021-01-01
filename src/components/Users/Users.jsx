fbeea0cce12668223d83258e9382593fb9951ecd
q <q>
2021-01-01 17:51:11 +0300

    2.2 componentDidMount (54)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,11 @@ import axios from "axios";
 import userPhoto from './1.jpg';
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                });
-
-
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
 
     render() {
